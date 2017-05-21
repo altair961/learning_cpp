d65c074593dcfcec17514a73051881bf5597ce31
altair961 <altair961@example.com>
2017-05-21 20:04:19 +0200

    Added "finding_school.cpp" file

---
+++ Chapter02/finding_school.cpp
@@ -0,0 +1,22 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Do 5 steps\n";
+	cout << "Turn right\n";
+	cout << "Enter the lift\n";
+	cout << "Go to the 0-th floor\n";
+	cout << "Go out of the lift\n";
+	cout << "Do four steps\n";
+	cout << "Open the door\n";
+	cout << "Do 4 steps\n";
+	cout << "Turn left\n";
+	cout << "Do 150 steps\n";
+	cout << "Turn right\n";
+	cout << "Do 400 steps\n";
+	cout << "Wait until green light on traffic lights appears\n";
+	cout << "Do 2000 steps\n";
+	cout << "Turn right\n";
+	cout << "Do 10 steps\n";
+	return 0;
+}
