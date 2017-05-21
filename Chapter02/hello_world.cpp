4116ea62a49c050f5c4fc6123b7a04acbb923b38
altair961 <altair961@example.com>
2017-05-21 18:15:43 +0200

    Fixed indentation

--- Chapter02/hello_world.cpp
+++ Chapter02/hello_world.cpp
@@ -1,8 +1,8 @@
 #include "std_lib_facilities.h"
 int main()
 {
-  cout << "Hello, programming on C++!\n";
-  cout << "Here we go!";
-  keep_window_open();
-  return 0;
+	cout << "Hello, programming on C++!\n";
+	cout << "Here we go!";
+	keep_window_open();
+	return 0;
 }
