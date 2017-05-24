44bf9e2a66a81a8881fe555d8183f88f8d455a4a
altair961 <altair961@example.com>
2017-05-24 23:06:55 +0200

    Changed to integer example

--- Chapter02/exercising_operators.cpp
+++ Chapter02/exercising_operators.cpp
@@ -2,15 +2,17 @@
 
 int main()
 {
-	cout << "Please enter a floating-point value: ";
-	double n;
+	cout << "Please enter an integer value: ";
+	int n;
 	cin >> n;
 	cout << "n == " << n
 		 << "\nn+1 == " << n+1
 		 << "\nthree times n == " << 3*n
 		 << "\ntwice n == " << n+n
 		 << "\nn squared == " << n*n
-		 << "\nhalf of n == " << n/2
+		 << "\ninteger division of n by 2 == " << n/2
+		 << "\nreminder of n devided to 2 == " << n%2
 		 << "\nsquare root of n == " << sqrt(n)
+		 << "\na/b * b + a%b == a: " << n/2 * 2 + n%2 
 		 << '\n';
 }
