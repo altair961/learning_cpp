66e8b7c86c0148a9b0bec6f4b48abee55252f838
renat_umitbaev <altair89@list.ru>
2017-05-27 19:41:04 +0200

    Added simple_form_letter.cpp example

---
+++ Chapter03/simple_form_letter.cpp
@@ -0,0 +1,11 @@
+#include "..\std_lib_facilities.h"
+int main()
+{
+	cout << "Please enter the name of the person you want write to\n";
+	string first_name = "???";
+	double age = -1;
+	cin >> first_name;
+	cout << "Dear " << first_name << ",\n";
+
+	return 0;
+}
