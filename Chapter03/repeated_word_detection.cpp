59d2144b6cf9b75cfc90a6ef1a591d9e9eafb77b
renat_umitbaev <altair89@list.ru>
2017-05-27 11:32:14 +0200

    Fixed a logical statement to avoid a bug

--- Chapter03/repeated_word_detection.cpp
+++ Chapter03/repeated_word_detection.cpp
@@ -3,10 +3,18 @@
 int main(){
 	string previous = ""; 
 	string current;
+	
 	while(cin>>current){
-		if(previous == current){
+		if(previous == current)
 			cout << "Repeated word: " + current << '\n';
+
 		previous = current;
-		}
+		
+		//else {
+		//	cout << "We are at else\n";
+		//	cout << "Current :" + current << '\n';
+		//	cout << "Previous :" + previous << '\n';
+		//}
+       
 	}
 }
