f44e8c57cbfb10c1c5c0f14f2395f3c017d5cd99
renat_umitbaev <altair89@list.ru>
2017-05-28 11:26:49 +0200

    Enabled to mention age and friends name in the text of the letter based on the input

--- Chapter03/simple_form_letter.cpp
+++ Chapter03/simple_form_letter.cpp
@@ -1,15 +1,37 @@
 #include "..\std_lib_facilities.h"
 int main()
 {
-	cout << "Please enter the name of the person you want write to\n";
 	string first_name = "???";
 	double age = -1;
 	char friend_sex = 0;
 	string friend_name = "???";
+	
+	cout << "Please enter the name of the person you want write to\n";	
 	cin >> first_name;
+	
 	cout << "\nEnter the name of your mutual friend/n";
 	cin >> friend_name;
+	
+	cout << "\nEnter m if the sex of the friend is male and f, if the sex of the friend is female: \n";
+	cin >> friend_sex;
+
+	cout << "\nEnter the age of the person you want write to\n";
+	
 	cout << "Dear " << first_name << ",\n\n";
 	cout << "How are you? I am fine. \nI miss you.\n\n";
 	cout << "Have you seen " << friend_name << " lately?";
+
+	if(friend_sex == m) {
+		cout << "If you see " + friend_name + " please ask him to call me\n";
+	}
+	
+	if(friend_sex == f) {
+		cout << "If you see " + friend_name + " please ask her to call me\n";
+	}
+
+	cout << "\nI hear you just had a birthday and you are " + age + " years old";
+
+	if((age == 0 || age < 0) || (age == 110) || (age > 110)) {
+		simple_error("you're kidding!");
+	}
 }
