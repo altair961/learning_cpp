2e51874ef98b174883c2f90038119704c0eb4399
renat_umitbaev <altair89@list.ru>
2017-05-28 09:49:36 +0200

    Added displaying of fiend_name variable

--- Chapter03/simple_form_letter.cpp
+++ Chapter03/simple_form_letter.cpp
@@ -4,7 +4,12 @@ int main()
 	cout << "Please enter the name of the person you want write to\n";
 	string first_name = "???";
 	double age = -1;
+	char friend_sex = 0;
+	string friend_name = "???";
 	cin >> first_name;
+	cout << "\nEnter the name of your mutual friend/n";
+	cin >> friend_name;
 	cout << "Dear " << first_name << ",\n\n";
-	cout << "How are you? I am fine. \nI miss you.";
+	cout << "How are you? I am fine. \nI miss you.\n\n";
+	cout << "Have you seen " << friend_name << " lately?";
 }
