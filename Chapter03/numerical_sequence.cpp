716a739c2de4af146fda65adc33e2b09392bced5
altair961 <altair89@list.ru>
2017-06-10 20:59:40 +0200

    Enabled to handle input with equal values

--- Chapter03/numerical_sequence.cpp
+++ Chapter03/numerical_sequence.cpp
@@ -1,3 +1,4 @@
+
 // Solution for exercise 3.6.
 // Show the numbers in numerical sequence
 
@@ -19,29 +20,29 @@ int main()
   cout << "Enter the third value: ";
   cin >> val3;
   
-  if(val1 < val2){
-    if(val1 < val3){
+  if(val1 <= val2){
+    if(val1 <= val3){
       cout << val1 << ", "; // show: val1
       cout << min(val2, val3) << ", "; //show: min(val2, val3);
-      cout << max(val2, val3); //show: max(val2, val3);
+      cout << max(val2, val3) << "\n"; //show: max(val2, val3);
     }
     if(val3 < val1){  //val1 < val2 && val3 < val1 show: val3, val1, val2
       cout << val3 << ", ";//show: val3
       cout << val1 << ", ";//show: val1
-      cout << val2; //show: val2
+      cout << val2 << "\n"; //show: val2
     }
   }
   
   if(val2 < val1){
-    if(val2 < val3){ 
-      cout << val2; //show: val2
-      cout << min(val1, val3); //show: min(val1, val3);
-      cout << max(val1, val3); //show: max(val1, val3);
+    if(val2 <= val3){ 
+      cout << val2 << ", "; //show: val2
+      cout << min(val1, val3) << ", "; //show: min(val1, val3);
+      cout << max(val1, val3) << "\n"; //show: max(val1, val3);
     }
     if(val3 < val2){ // val3, val2, val1
       cout << val3 << ", "; //show: val3
       cout << val2 << ", "; //show: val2
-      cout << val1; //show: val1
+      cout << val1 << "\n"; //show: val1
     }   
   }
 }
@@ -54,7 +55,9 @@ int min(int first, int second)
   if(second < first){
     return second;
   }
-    
+  if(first == second){
+    return first;
+  }
   return -1;
 }
 
@@ -66,6 +69,8 @@ int max(int first, int second)
   if(second > first){
     return second;
   }
-
+  if(first == second){
+    return first;
+  }
   return -1;
 }
