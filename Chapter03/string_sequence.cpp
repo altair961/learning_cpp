39d8c6c3cae6261caf48e462245974c3ea94ddf2
altair961 <altair89@list.ru>
2017-06-11 14:14:12 +0200

    Enabled to_lower function to operate on strings rather than single chars

--- Chapter03/string_sequence.cpp
+++ Chapter03/string_sequence.cpp
@@ -3,7 +3,7 @@
 
 #include "../std_lib_facilities.h"
 
-char to_lower(char str);
+string to_lower(string str);
 
   int const first_ascii_uppercase = 65;
   int const last_ascii_uppercase = 90;
@@ -21,17 +21,31 @@ int main()
   cin >> first_str;
   //cout << first_str[0];
 
-  cout << to_lower(first_str[0]) << "\n";
+  //  cout << to_lower(first_str[0]) << "\n";
 
+  cout << to_lower(first_str) << "\n";
   
   //get 1-st char of string 
   //to lowercase before start use own tolower function
   return 0;
 }
 
-char to_lower(char curr_char){
+string to_lower(string str){
   //  str[0] = 99;
-
+  int char_alphabet_pos = 0;
+  for (int i = 0; i < str.length(); i++) {
+    char curr_char = str[i];
+    
+    if(curr_char >= first_ascii_uppercase && curr_char <= last_ascii_uppercase){
+      char_alphabet_pos = curr_char - first_ascii_uppercase; 
+    
+    //  str[curr_char] = first_ascii_lowercase + char_alphabet_pos;
+      curr_char = first_ascii_lowercase + char_alphabet_pos;
+      str[i] = curr_char;
+    }
+  }
+  return str;
+  /*
   int char_alphabet_pos = 0;
   if(curr_char >= first_ascii_uppercase && curr_char <= last_ascii_uppercase){
     char_alphabet_pos = curr_char - first_ascii_uppercase; 
@@ -40,6 +54,7 @@ char to_lower(char curr_char){
     //  str[curr_char] = first_ascii_lowercase + char_alphabet_pos;
     curr_char = first_ascii_lowercase + char_alphabet_pos;
   }
+  */
+  //  return curr_char;
   
-  return curr_char;
 }
