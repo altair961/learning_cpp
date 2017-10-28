2718e72eff6552b8f791f3248befff84ffbf8bf7
altair961 <altair89@list.ru>
2017-10-29 00:20:39 +0300

    Cleaned-up

--- Chapter02/hello_world.cpp
+++ Chapter02/hello_world.cpp
@@ -1,9 +1,9 @@
-#include "..\std_lib_facilities.h"
+#include "../std_lib_facilities.h"
 
 int main()
 {
-	cout << "Hello, programming on C++!\n";
-	cout << "Here we go!";
-	keep_window_open();
-	return 0;
+  cout << "Hello, programming on C++!\n";
+  cout << "Here we go!";
+  keep_window_open();
+  return 0;
 }
