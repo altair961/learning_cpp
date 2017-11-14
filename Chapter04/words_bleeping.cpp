7299baf1ac02da82fffebe375704efc38859f0b0
altair961 <altair961@github.com>
2017-11-14 17:49:04 +0100

    Enabled words_bleeping to use a set of words to be bleeped out instead of one

--- Chapter04/words_bleeping.cpp
+++ Chapter04/words_bleeping.cpp
@@ -1,11 +1,15 @@
+// This program takes in words from the keyboard and then shows them with
+// prohibited words bleeped out.
+
 #include "..\std_lib_facilities.h"
 
-void bleep_out(vector<string>, string, string);
+void bleep_out(vector<string>, string, vector<string>);
 
 int main()
 {
   vector<string> words;
-  string disliked = "Broccoli";
+  vector<string> disliked = { "broccoli", "Broccoli", "storm", "computer" };
+  //  string disliked = "Broccoli";
   string bleep_sign = "BLEEP";
   cout << "Please, input words" << endl;
 
@@ -15,13 +19,12 @@ int main()
   bleep_out(words, bleep_sign, disliked);
 }
 
-void bleep_out(vector<string> words, string bleep_sign, string disliked)
+void bleep_out(vector<string> words, string bleep_sign, vector<string> disliked)
 {
   for (int i = 0; i < words.size(); i++)
-  {
-    if(words[i] == disliked)
-      words[i] = bleep_sign;
-  }
+    for (int j = 0; j < disliked.size(); j++)
+      if (words[i] == disliked[j])
+	words[i] = bleep_sign;
 
   for(string word : words)
     cout << word << endl;
