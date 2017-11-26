e24d5b1bea83dd8234d1fd3990c61db3b57a51bd
altair961 <altair961@github.com>
2017-11-26 13:43:20 +0400

    changed ints to doubles

--- Chapter04/number_cruncher.cpp
+++ Chapter04/number_cruncher.cpp
@@ -1,34 +1,39 @@
 #include "..\std_lib_facilities.h"
-void show_result(int, int);
+void show_result(double, double);
 
 int main()
 {
-  int input_1 = 0;
-  int input_2 = 0;
-  int input_3 = 0;
+  double input_1 = 0;
+  double input_2 = 0;
+  double input_3 = 0;
   
-  int smallerValue = 0;
-  int largerValue = 0;
+  double smallerValue = 0;
+  double largerValue = 0;
   
   cout << "Please, provide two integer numbers" << endl
        << "When input is not a number it is being assigned to 0" << endl
        << "Terminate the input with '|'" << endl;
-  
+
+  in
+  int input_2_int;
+  int input_3_int;
   
   while ((cin >> input_1 &&
       	  cin >> input_2 &&
      	  cin >> input_3) &&
           !((char(input_1) == '|') ||
-	    (char(input_2) == '|') ||
+	    (char(myInt = input_2) == '|') ||
 	    (char(input_3) == '|')))
   {    
     cout << input_1 << "\n";
     cout << input_2 << "\n";
   }
 
+  cout << "hey";
+  
   if(input_1 > input_2)
   {
-    smallerValue = input_2;
+    smallerValue = input_2;    
     largerValue = input_1;
 
     show_result(smallerValue, largerValue);
@@ -51,7 +56,7 @@ int main()
   return 0;
 }
 
-void show_result(int smallerValue, int largerValue)
+void show_result(double smallerValue, double largerValue)
 {
   cout << "The smaller value is: " << smallerValue << endl
        << "The larger value is: " << largerValue << endl;
