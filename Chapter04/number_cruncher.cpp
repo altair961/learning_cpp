c49b92a2078ea4cba42bbdee02cbab8afa1d8dc3
altair961 <altair961@github.com>
2017-12-03 14:38:54 +0300

    fixed comparing

--- Chapter04/number_cruncher.cpp
+++ Chapter04/number_cruncher.cpp
@@ -1,63 +1,77 @@
 #include "..\std_lib_facilities.h"
 
+bool IsUnitAcceptable(string);
+bool IsInputSucceeded(double, string);
+bool IsInputTerminated(double);
+
 int main()
 {
-  double input_1 = 0;
-  double input_2 = 0;
-  double input_3 = 0;
-  int truncated_1 = 0;
-  int truncated_2 = 0;
-  int truncated_3 = 0;
-  double smallerValue = 0;
-  double largerValue = 0;
+  double input_value = 0.0;
+  double largest_so_far = 0;
+  double smallest_so_far = 0;
+  string unit = "";
   
   cout << "Please, provide two numbers" << endl
        << "When input is not a number it is being assigned to 0" << endl
        << "Terminate the input with '|'" << endl;
-
-  int input_2_int;
-  int input_3_int;
   
-  while ((cin >> input_1 &&
-      	  cin >> input_2 &&
-     	  cin >> input_3) &&
-          !((char(truncated_1 = input_1) == '|') ||
-	    (char(truncated_2 = input_2) == '|') ||
-	    (char(truncated_3 = input_3) == '|')))
-  {    
-    cout << input_1 << "\n";
-    cout << input_2 << "\n";
+  while (cin >> input_value &&
+    //	 cin >> unit) &&
+	 //	 IsUnitAcceptable(unit))// &&
+    	 !IsInputTerminated(input_value))
+  {
+    cout << "======================================" << endl;
+    cout << "Inputed number: " << input_value << endl << unit;
+    smallest_so_far = input_value;
+    if(largest_so_far < input_value)
+    {
+      smallest_so_far = largest_so_far;
+      largest_so_far = input_value;
+      cout << "The largest so far is: " << largest_so_far << endl;
+      cout << "The smallest so far is: " << smallest_so_far << endl;
+    }
+    if(largest_so_far > input_value)
+    {
+      smallest_so_far = input_value;
+      cout << "The largest so far is: " << largest_so_far << endl;
+      cout << "The smallest so far is: " << smallest_so_far << endl;
+    }
+    if(largest_so_far == smallest_so_far)
+    {
+      cout << "The largest so far is: " << largest_so_far << endl;
+      cout << "The smallest so far is: " << smallest_so_far << endl;
+      cout << "The numbers are equal" << endl;
+    }
+   
+    if(((largest_so_far - smallest_so_far) <= 1.0/100) &&
+       ((largest_so_far - smallest_so_far) != 0.0))
+    {
+     cout << "The numbers are almost equal" << endl
+	  << "smallest_so_far: " << smallest_so_far << endl
+	  << "largest_so_far: " << largest_so_far << endl;
+    }
+    cout << "The largest so far is: " << largest_so_far << endl;
+      cout << "The smallest so far is: " << smallest_so_far << endl;
   }
-
-  cout << "hey";
+    cout << "smallest_so_far: " << smallest_so_far << endl;
+   cout << "largest_so_far: " << largest_so_far << endl;
   
-  if(input_1 > input_2)
-  {
-    smallerValue = input_2;    
-    largerValue = input_1;
-
-    cout << "The smaller value is: " << smallerValue << endl
-	 << "The larger value is: " << largerValue << endl;
-  }
-  if(input_1 < input_2)
-  {
-    smallerValue = input_1;
-    largerValue = input_2;
-
-    cout << "The smaller value is: " << smallerValue << endl
-	 << "The larger value is: " << largerValue << endl;
-  }
-  if(input_1 == input_2)
-  {
-    cout << "The numbers are equal" << endl;
-  }
-  if((input_1 - input_2) || (input_2 - input_1) <= 1.0/100)
-  {
-    cout << "The numbers are almost equal" << endl;
-  }
   
-  cout << input_1 << endl;
-  cout << input_2 << endl;
-
+ //  cout << smallest_so_far << endl;
+ //  cout << largest_so_far << endl;
+  
   return 0;
+}
+
+bool IsUnitAcceptable(string unit)
+{
+  if(unit == "cm" || unit == "m" || unit == "in" || unit == "ft")
+    return true;
+  cout <<"at IsUnitAcceptable";
+  return false;
+}
+
+bool IsInputTerminated(double input_value)
+{
+  return char(input_value) == '|';
 }
