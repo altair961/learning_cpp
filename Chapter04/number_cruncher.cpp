0ccae97ebe1c4d9202e1fd3426fca7250708ca4b
altair961 <altair961@github.com>
2017-12-08 08:01:49 +0300

    Enabled to get doubles with units as input

--- Chapter04/number_cruncher.cpp
+++ Chapter04/number_cruncher.cpp
@@ -2,24 +2,62 @@
 
 bool IsUnitAcceptable(string);
 bool IsInputSucceeded(double, string);
-bool IsInputTerminated(double);
+bool IsInputTerminated(char);
+string GetUnit(string);
 
 int main()
 {
-  double input_value = 0.0;
+  vector<char> input;
+
   double largest_so_far = 0;
   double smallest_so_far = 0;
-  string unit = "";
+  //  string unit = "";
   
   cout << "Please, provide two numbers" << endl
        << "When input is not a number it is being assigned to 0" << endl
        << "Terminate the input with '|'" << endl;
+
+  double current_char = 0;
+  char unit = 0;
+
+  while (cin >> current_char &&
+         cin >> unit &&
+	 (current_char != '|' && unit != '|' )) // &&
+    //       !(current_char == '|' || temp == '|'))
+  {  
+    input.push_back(current_char); // change to array for doubles
+    input.push_back(unit); 
+
+    cout << current_char << endl;
+    cout << unit << endl;
+    //    for(char c : input)
+      //    {
+      //      cout << c << endl;
+      //    }
+    cout << "End of iteration" << endl;
+  }
   
+  /*
+  for (int i = 0; i <= 2; i++)
+  {
+    cin >> current_char;
+    cin >> temp;
+
+    input.push_back(current_char);
+    input.push_back(tmp);
+    
+    cout << endl << "First iteration has been finished";
+  }*/
+
+  cout << input[0] << endl << input[1];
+  
+  /*
   while (cin >> input_value &&
     //	 cin >> unit) &&
-	 //	 IsUnitAcceptable(unit))// &&
+	 //	 IsUnitAcceptable(GetUnit(input_value)) &&
     	 !IsInputTerminated(input_value))
   {
+    cout << input_value[0] << endl;
     cout << "======================================" << endl;
     cout << "Inputed number: " << input_value << endl << unit;
     smallest_so_far = input_value;
@@ -59,19 +97,29 @@ int main()
   
  //  cout << smallest_so_far << endl;
  //  cout << largest_so_far << endl;
-  
+ */   
   return 0;
 }
 
 bool IsUnitAcceptable(string unit)
 {
+  cout << "unit: " << unit << endl;
   if(unit == "cm" || unit == "m" || unit == "in" || unit == "ft")
     return true;
-  cout <<"at IsUnitAcceptable";
+  cout <<"at IsUnitAcceptable" << endl;
   return false;
 }
 
-bool IsInputTerminated(double input_value)
+bool IsInputTerminated(char current_char)
 {
-  return char(input_value) == '|';
+  return char(current_char) == '|';
+}
+
+string GetUnit(string input)
+{
+  for(int x : input)
+  {
+    
+  }
+  return "hey";
 }
