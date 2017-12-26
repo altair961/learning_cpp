ab932f4d77cb4d4a82c666db0acac1380c28e69b
altair961 <altair961@github.com>
2017-12-26 18:28:48 +0300

    Added cities_distances.cpp

--- Chapter04/cities_distances.cpp
+++ Chapter04/cities_distances.cpp
@@ -1,9 +1,44 @@
 #include "..\std_lib_facilities.h"
 
-vector<double> dist_between_two_cities;
-double distances_sum;
+vector<double> distances;
+double total_distance = 0.0;
+double get_smallest(vector<double>);
+double get_greatest(vector<double>);
+double get_mean(vector<double>);
+int zero_index_conpensator = 1;
 
 int main()
 {
+  cout << "Please, enter values: " << endl;
+  for (double distance; cin >> distance;)
+  {
+    distances.push_back(distance);
+        total_distance = total_distance + distance;
+  }
+
+  cout << "The smallest number: " << get_smallest(distances) << endl;
+  cout << "The greatest number: " << get_greatest(distances) << endl;
+  cout << "The mean is: " << get_mean(distances) << endl;
+  cout << "The total distance: " << total_distance << endl;
+
   return 0;
+}
+
+double get_smallest(vector<double> distances)
+{
+  sort(distances);
+  return distances.at(0);
+}
+
+double get_greatest(vector<double> distances)
+{
+  sort(distances);  
+  return distances.at(distances.size() - zero_index_conpensator);
+}
+
+double get_mean(vector<double> distances)
+{
+  int last_element_index = distances.size() - zero_index_conpensator;
+  int first_element_index = 0;
+  return (distances.at(last_element_index) + distances.at(first_element_index)) / 2;
 }
