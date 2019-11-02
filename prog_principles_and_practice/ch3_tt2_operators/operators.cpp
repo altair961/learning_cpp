cf50f0dd0213d1e386c911cd87bbb3abe33d6b26
altair961 <altair89@list.ru>
2019-11-02 17:40:04 +0300

    Modified "operators" example for int instead of double

--- prog_principles_and_practice/ch3_tt2_operators/operators.cpp
+++ prog_principles_and_practice/ch3_tt2_operators/operators.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	cout << "Please enter a floating-point value: ";
-	double n;
+	int n;
 	cin >> n;
 	cout << "n == " << n
 		<< "\nn+1 == " << n + 1
@@ -12,6 +12,7 @@ int main()
 		<< "\ntwice n == " << n + n
 		<< "\nn squared == " << n * n
 		<< "\nhalf of n == " << n / 2
+		<< "\nmodulo of n == " << n % 2
 		<< "\nsquare root of n == " << sqrt(n)
 		<< "\n";  // another name for newline (“end of line”) in output
 }
