599758cbe1f877b602effe8ef3df9751eda2cdc2
altair961 <altair89@list.ru>
2020-05-05 13:19:30 +0300

    Created a window for 3 seconds.

---
+++ tanks/tanks/Game.cpp
@@ -0,0 +1,13 @@
+#include "SDL.h"
+#include <stdio.h>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int heightm bool fu)
+private:
+
+};
