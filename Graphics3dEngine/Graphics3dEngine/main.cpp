6d1066f5ee75b3b278ed66e4c1fcee38dd22a29f
altair961 <altair89@list.ru>
2020-09-24 23:40:28 +0300

    Initialized PixelGameEngine

--- Graphics3dEngine/Graphics3dEngine/main.cpp
+++ Graphics3dEngine/Graphics3dEngine/main.cpp
@@ -1,45 +1,31 @@
-//#define OLC_PGE_APPLICATION
-//#include "olcPixelGameEngine.h"
-//
-////class Graphics3dEngine : olc::PixelGameEngine
-////{
-////
-////}
-
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-// Override base class with your custom functionality
-class Example : public olc::PixelGameEngine
+class Graphics3dEngine : public olc::PixelGameEngine
 {
 public:
-	Example()
+	Graphics3dEngine()
 	{
-		// Name you application
-		sAppName = "Example";
+		sAppName = "Graphics3dEngine";
 	}
 
 public:
 	bool OnUserCreate() override
 	{
-		// Called once at the start, so create things here
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame, draws random coloured pixels
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
 		return true;
 	}
 };
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
-		demo.Start();
+	Graphics3dEngine graphics3dEngine;
+	if (graphics3dEngine.Construct(256, 240, 4, 4))
+		graphics3dEngine.Start();
+
 	return 0;
 }
