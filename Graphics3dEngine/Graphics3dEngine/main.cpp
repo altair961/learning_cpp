2ff049535f47197d9bbdb8db6b8003b3f9bd7127
altair961 <altair89@list.ru>
2020-09-29 23:55:53 +0300

    Populated projection matrix with elements. Created MultiplyMatrixVector method, that gets input 3D point, gets projection matrix and returns output 3D point

--- Graphics3dEngine/Graphics3dEngine/main.cpp
+++ Graphics3dEngine/Graphics3dEngine/main.cpp
@@ -32,6 +32,20 @@ public:
 
 private:
 	mesh meshCube;
+	mat4x4 matProj;
+	void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m) // input 3D point, transformed 3D point, transformation or projection matrix 
+	{
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3]; // this element appeared in our tranformation matrix due to the fact that there is a gap between user's head and near plane of view frustum
+
+		// Now we on 4D, in order to come back to 3D cartesian space we have to divide by w
+		if(w != 0)
+		{
+			o.x /= w; o.y /= w; o.z /= w;
+		}
+	}
 
 public:
 	bool OnUserCreate() override
@@ -71,7 +85,15 @@ public:
 		float fFar = 1000.0f;
 		float fFov = 90.0f; // fov stands for field of view in degrees
 		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
-		float fFovRad = 1.0f / tanf(fFov * 0.5f / 100.0f * 3.14159f);
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 100.0f * 3.14159f); // tangent calculation of theta/2 angle i.e. our scaling factor
+
+		//       [y][x]
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
 
 		return true;
 	}
