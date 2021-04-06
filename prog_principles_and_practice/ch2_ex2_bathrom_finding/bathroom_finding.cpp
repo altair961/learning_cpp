db356cb310c219e9ab43d1a70da87beb15d973d9
altair961 <altair89@list.ru>
2021-04-07 00:16:53 +0300

    ch2_ex1_hello_world ch2_ex2_bathrom_finding and ch2_ex3_getting_to_school have been added

---
+++ prog_principles_and_practice/ch2_ex2_bathrom_finding/bathroom_finding.cpp
@@ -0,0 +1,17 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	cout << "stand up" << endl;
+	cout << "turn right" << endl;
+	cout << "one step forward" << endl;
+	cout << "turn left" << endl;
+	cout << "three steps forward" << endl;
+	cout << "go upstairs one step at a time" << endl;
+	cout << "when front is blocked try open a door" << endl;
+	cout << "when door is opened enter the bathroom" << endl;
+
+
+	keep_window_open();
+	return 0;
+}
---
+++ prog_principles_and_practice/ch2_ex3_getting_to_school/getting_to_school.cpp
@@ -0,0 +1,14 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	cout << "exit through the door" << endl;
+	cout << "use lift to reach first floor" << endl;
+	cout << "go striaght along the road" << endl;
+	cout << "turn left" << endl;
+	cout << "three steps forward" << endl;
+	cout << "open the school door" << endl;
+
+	keep_window_open();
+	return 0;
+}
