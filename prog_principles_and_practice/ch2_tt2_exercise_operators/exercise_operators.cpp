1a375fdf8ae96a93998cbf872512fb8bcf0830bd
altair961 <altair89@list.ru>
2021-04-28 20:49:57 +0200

    Input variable type has been changed

--- prog_principles_and_practice/ch2_tt2_exercise_operators/exercise_operators.cpp
+++ prog_principles_and_practice/ch2_tt2_exercise_operators/exercise_operators.cpp
@@ -2,15 +2,17 @@
 
 int main()
 {
-	cout << "Please enter a floating-point value: ";
-	double n;
-	cin >> n;
-	cout << "n == " << n
-		<< "\nn+1 == " << n+1
-		<< "\nthree times n == " << 3*n
-		<< "\ntwice n == " << n+n
-		<< "\nsquared n == " << n*n
-		<< "\nhalf of n == " << n/2
-		<< "\nsquare root of n == " << sqrt(n)
+	cout << "Please enter an integer value: ";
+	int inputInt;
+	cin >> inputInt;
+	double inputDouble = inputInt;
+
+	cout << "n == " << inputInt
+		<< "\nn+1 == " << inputInt+1
+		<< "\nthree times n == " << 3*inputInt
+		<< "\ntwice n == " << inputInt+inputInt
+		<< "\nsquared n == " << inputInt*inputInt
+		<< "\nhalf of n == " << inputInt/2
+		<< "\nsquare root of n == " << sqrt(inputDouble)
 		<< '\n';
 }
