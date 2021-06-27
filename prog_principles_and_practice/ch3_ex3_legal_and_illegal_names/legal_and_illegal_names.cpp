f6459e4c8fa35b5c84c45f96bd6ecc5ceab167c1
altair961 <altair89@list.ru>
2021-06-27 14:31:11 +0200

    legal_and_illegal_names.cpp has been implemented

---
+++ prog_principles_and_practice/ch3_ex3_legal_and_illegal_names/legal_and_illegal_names.cpp
@@ -0,0 +1,13 @@
+int main() {
+	// int double = 0;
+	// int This_little_pig;
+	// int latest thing;
+	// int MiniMineMine;
+	// int This_1_is fine = 0;
+	// int the_$12_method = 0;
+	// int number;
+	// int 2_For_1_special = 0;
+	// int _this_is_ok = 0;
+	// bool correct? = false;
+	return 0;
+}
