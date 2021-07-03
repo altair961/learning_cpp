0305da8c33f6bdda0071d0809db269d12d2faa44
altair961 <altair89@list.ru>
2021-07-03 12:02:09 +0200

    Some folders have been renamed. ch3_ex7_string_values_sorter.cpp has been implemented

---
+++ prog_principles_and_practice/ch3_ex7_string_values_sorter/ch3_ex7_string_values_sorter.cpp
@@ -0,0 +1,47 @@
+#include "../std_lib_facilities.h"
+
+int main() 
+{
+	string val1 = "";
+        string val2 = "";
+        string val3 = "";
+        cout << "Please enter three values, separated by enter: \n";
+        cin >> val1;
+        cin >> val2;
+        cin >> val3;
+
+        if(val1 <= val2 && val1 <= val3) // val1 is smallest
+        {
+                cout << val1 << ", ";
+                if(val2 <= val3)
+                {
+                        cout << val2 << ", " << val3 << "\n";
+                } else if (val3 < val2)
+                {
+                        cout << val3 << ", " << val2 << "\n";
+                }
+        } else if(val2 < val1 && val2 < val3) // val2 is smallest
+        {
+                cout << val2 << ", ";
+                if(val1 <= val3)
+                {
+                        cout << val1 << ", " << val3 << "\n";
+                } else if(val3 < val1)
+                {
+                        cout << val3 << ", " << val1 << "\n";
+                }
+        } else if(val3 < val1 && val3 < val2) // val3 is smallest
+        {
+                cout << val3 << ", ";
+                if(val1 <= val2)
+                {
+                        cout << val1 << ", " << val2 << "\n";
+                } else if(val2 < val1)
+                {
+                        cout << val2 << ", " << val1 << "\n";
+                }
+        }
+
+
+	return 0;
+}
