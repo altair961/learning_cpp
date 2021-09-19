c98e18e6da1b14e5e66ab3c706c8b71548b33da8
altair961 <altair89@list.ru>
2021-09-19 19:54:00 +0200

    Enabled interface based registering or 'binding'

--- TanksCppTdd.UnitTests/TanksCppTdd.UnitTests/Source.cpp
+++ TanksCppTdd.UnitTests/TanksCppTdd.UnitTests/Source.cpp
@@ -7,18 +7,29 @@ struct renderer {
 	int device;
 };
 
-class view {
+class iview {
 public:
-	view(std::string /*title*/, const renderer&) {}
+	virtual ~iview() noexcept = default;
+	virtual void update() = 0;
 };
 
+class gui_view : public iview {
+public:
+	gui_view(std::string title, const renderer&) {}
+	void update() override {}
+};
+
+class text_view : public iview {
+public:
+	void update() override {}
+};
 class model {};
 
 class controller {
 private:
 	int distanceMeters = 5;
 public:
-	controller(model&, view&) {}
+	controller(model&, iview&) {}
 };
 
 class user {};
@@ -48,6 +59,10 @@ int main() {
 	 * app app_{controller_, user_};
 	 */
 
-	auto injector = di::make_injector();
-	auto appl = injector.create<app>();
+	auto injector = di::make_injector(
+		di::bind<iview>().to<gui_view>()
+		, di::bind<int>().to(42) // renderer device
+	);
+
+	injector.create<app>();
 }
