2436eac44d62e3cae43723ffb71617aa2fdd140c
altair961 <altair89@list.ru>
2021-09-19 22:30:59 +0200

    Added Tanks project structure

---
+++ Tanks/Tanks/Tanks.Game.UnitTests/GameTests.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+
+
+TEST_CASE("When Game ctor invoked and graphicsEngine is set to NULL should throw invalid argument exception", "[factorial]") {
+
+    REQUIRE(1 == 1);
+}
+
+
+TEST_CASE("Factorials are computeddkjbfsdlkjb", "[factorial]") {
+    REQUIRE(1 == 1);
+}
---
+++ Tanks/Tanks/Tanks.GraphicsEngine.UnitTests/GraphicsEngineTests.cpp
@@ -0,0 +1,9 @@
+#include "catch.hpp"
+
+TEST_CASE("Factorials are computedjkvfbkdvfb", "[factorial]") {
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("Factorials are dsmvf", "[factorial]") {
+    REQUIRE(1 == 1);
+}
