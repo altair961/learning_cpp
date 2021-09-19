fa3f9c68dcb1964da2437c85e04a980463412b13
altair961 <altair89@list.ru>
2021-09-19 23:54:07 +0200

    Defined a test for Game.Launch

--- Tanks/Tanks/Tanks.cpp
+++ Tanks/Tanks/Tanks.cpp
@@ -1,33 +1,45 @@
 #include "di.hpp"
 #include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
 
 namespace di = boost::di;
 
 class ILaunch {
 public:
 	virtual ~ILaunch() noexcept = default;
-	virtual void Launch() = 0;
+	virtual void Launch(std::string name) = 0;
 };
 
 class Game : public ILaunch {
 public:
 	Game(std::string title) {}
-	void Launch() override 
+	void Launch(std::string name) override 
 	{
-		if (true)
+
+		if (name == "")
 		{
-			std::cout << "Game Launched!" << std::endl;
+			throw new std::invalid_argument("name string parameter is empty");
 		}
 	}
 };
+//
+//int main()
+//{
+//	auto injector = di::make_injector(
+//		di::bind<ILaunch>().to<Game>()
+//	);
+//
+//	auto game = injector.create<Game>();
+//	game.Launch();
+//}
 
-int main()
-{
+TEST_CASE("When Game is launched with empty string should throw invalid_argument exception", "[factorial]") {
 	auto injector = di::make_injector(
-		di::bind<ILaunch>().to<Game>()
-	);
+	di::bind<ILaunch>().to<Game>());
 
 	auto game = injector.create<Game>();
-	game.Launch();
-}
 
+	REQUIRE_THROWS_AS(game.Launch(""), std::invalid_argument);
+}
