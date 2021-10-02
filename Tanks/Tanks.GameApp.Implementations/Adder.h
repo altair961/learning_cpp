4cd9cef0b5290cbfb7711e0673e1bc1f435411ab
altair961 <altair89@mail.ru>
2021-10-02 10:34:12 +0200

    Replaced Tanks with an empty solution, created in VS2022

--- Tanks/Tanks.GameApp.Implementations/Adder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "TanksGameAppInterfaces.h" 
-
-class Adder : IAdd
-{
-public:
-    int Add(int, int);
-};
--- Tanks/Tanks.GameApp.Interfaces/IAdd.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class IAdd {
-public:
-//    virtual ~IAdd() noexcept = default;
-    virtual int Add(int firstNumber, int secondNumber) = 0;
-};
