5ee1c626e2df67e29a68c1bb8ff162992a0459fb
altair961 <altair89@list.ru>
2021-10-04 23:48:08 +0200

    Window and game loop have been implemented

--- Tanks/Tanks.GameApp.Client/Client.cpp
+++ Tanks/Tanks.GameApp.Client/Client.cpp
@@ -1,11 +1,22 @@
+#include "Game.h"
+
+Game* game = nullptr;
+
 int main(int argc, char *argv[])
 {
-	while (true) //game loop while game is running
+	game = new Game();
+	game->init("Tanks", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	while (game->running()) //game loop while game is running
 	{
+		game->handleEvents();
+		game->update();
+		game->render();
 		// handle any input
 		// update all object eg. positions, etc
 		// render changes to the display
 	}
+
+	game->clean();
 
 	return 0;
 }
