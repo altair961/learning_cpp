0160876055af7064a68a0646db8adb84887b7055
altair961 <altair89@list.ru>
2021-10-09 00:39:27 +0200

    Smooth moving enabled. Movement speed does not depend on the computer speed now.

--- Tanks/Tanks.GameApp.Client/Client.cpp
+++ Tanks/Tanks.GameApp.Client/Client.cpp
@@ -4,16 +4,31 @@ Game* game = nullptr;
 
 int main(int argc, char *argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("Tanks", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 	while (game->running()) //game loop while game is running
 	{
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
 		// handle any input
 		// update all object eg. positions, etc
 		// render changes to the display
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
