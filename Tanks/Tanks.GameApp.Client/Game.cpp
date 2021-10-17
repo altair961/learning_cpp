1d52981b12fdb1786d04d9c5ca3a3a4ab0137c72
firebat961 <altair89@list.ru>
2021-10-17 22:29:38 +0200

    Sprite component has been added.

--- Tanks/Tanks.GameApp.Client/Game.cpp
+++ Tanks/Tanks.GameApp.Client/Game.cpp
@@ -1,13 +1,9 @@
 #include "Game.h"
 #include "TextureManager.h"
-#include "GameObject.h"
 #include "Map.h"
+#include "ECS/Components.h"
 
-#include "ECS.h"
-#include "Components.h" 
 
-GameObject* player;
-GameObject* enemy;
 Map* map;
 
 SDL_Renderer* Game::renderer = nullptr;
--- Tanks/Tanks.GameApp.Client/GameObject.cpp
+++ Tanks/Tanks.GameApp.Client/GameObject.cpp
@@ -13,15 +13,15 @@ void GameObject::Update()
 {
 	xpos++;
 	ypos++;
-	srcRect.h = 64;
-	srcRect.w = 64;
+	srcRect.h = 32;
+	srcRect.w = 32;
 	srcRect.x = 0;
 	srcRect.y = 0;
 
 	destRect.x = xpos;
 	destRect.y = ypos;
-	destRect.w = srcRect.w;
-	destRect.h = srcRect.h;
+	destRect.w = srcRect.w * 2;
+	destRect.h = srcRect.h * 2;
 }
 
 void GameObject::Render() 
