161acd56d2937f47f1cd65fbabcc16b2b5e971ec
firebat961 <altair89@list.ru>
2021-10-18 20:52:47 +0200

    Assets have been rearranged

--- ConsoleTetris/ConsoleTetris/ConsoleTetris.cpp
+++ ConsoleTetris/ConsoleTetris/ConsoleTetris.cpp
@@ -13,33 +13,33 @@ int main()
 
 	tetromino[1].append(L"..X.");
 	tetromino[1].append(L".XX.");
-	tetromino[1].append(L"..X.");
+	tetromino[1].append(L".X..");
 	tetromino[1].append(L"....");
-	
-	tetromino[2].append(L"....");
+
+	tetromino[2].append(L".X..");
 	tetromino[2].append(L".XX.");
-	tetromino[2].append(L".XX.");
+	tetromino[2].append(L"..X.");
 	tetromino[2].append(L"....");
-	
-	tetromino[3].append(L"..X.");
+
+	tetromino[3].append(L"....");
 	tetromino[3].append(L".XX.");
-	tetromino[3].append(L".X..");
+	tetromino[3].append(L".XX.");
 	tetromino[3].append(L"....");
 
-	tetromino[4].append(L".X..");
+	tetromino[4].append(L"..X.");
 	tetromino[4].append(L".XX.");
 	tetromino[4].append(L"..X.");
 	tetromino[4].append(L"....");
 
-	tetromino[5].append(L".X..");
-	tetromino[5].append(L".X..");
-	tetromino[5].append(L".XX.");
 	tetromino[5].append(L"....");
-
-	tetromino[6].append(L"..X.");
-	tetromino[6].append(L"..X.");
-	tetromino[6].append(L".XX.");
+	tetromino[5].append(L".XX.");
+	tetromino[5].append(L"..X.");
+	tetromino[5].append(L"..X.");
+	
 	tetromino[6].append(L"....");
+	tetromino[6].append(L".XX.");
+	tetromino[6].append(L".X..");
+	tetromino[6].append(L".X..");
 
 	return 0;
 }
