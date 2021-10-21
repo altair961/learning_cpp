3dabf4c40733a56e8c5f093b28b77436df0465ec
firebat961 <altair89@list.ru>
2021-10-21 23:41:31 +0200

    Copy constructor and copy operator have been deleted, because we adhere to RAII.

--- LittleVulkanEngine/LittleVulkanEngine/LittleVulkanEngine/lve_window.h
+++ LittleVulkanEngine/LittleVulkanEngine/LittleVulkanEngine/lve_window.h
@@ -10,6 +10,12 @@ namespace lve { // lve means little vulkan engine
 		LiveWindow(int w, int h, std::string name);
 		~LiveWindow();
 
+		// We delete copy constructor and copy operator. This is because we are using a pointer to our glfw window
+		// We adhere to RAII principle. RAII means, that resource creation happens when we initialize our variable and 
+		// their cleanups are performed by their distructors
+		LiveWindow(const LiveWindow&) = delete;
+		LiveWindow& operator=(const LiveWindow&) = delete;
+
 		bool shouldClose() { return glfwWindowShouldClose(window); }
 
 	private:
