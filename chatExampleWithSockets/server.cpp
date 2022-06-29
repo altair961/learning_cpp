b5321dfd9512ea259b49d32163eb72251a6054a0
altair961 <altair89@list.ru>
2022-06-29 19:12:47 +0300

    includes were added to server.cpp

---
+++ chatExampleWithSockets/server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main() {
+        std::cout << "dsfhbsdjvhb" << std::endl;
+}
