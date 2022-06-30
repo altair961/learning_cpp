f35eadd6cf6f4bb181007ada5568300988074988
altair961 <altair89@list.ru>
2022-06-30 09:07:03 +0300

    Makefile was defined. Basic code was defined for server.cpp

--- chatExampleWithSockets/server.cpp
+++ chatExampleWithSockets/server.cpp
@@ -1,10 +1,20 @@
+#include <unistd.h>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
 
-int main() {
-        std::cout << "dsfhbsdjvhb" << std::endl;
+int main(int argc, char const* argv[]) {
+	int client;
+	int server;
+
+	struct sockaddr_in server_address;
+
+	client = socket(AF_INET, SOCK_STREAM, 0);
+
+	std::cout << "dsfhbsdjvhb" << std::endl;
 }
+
