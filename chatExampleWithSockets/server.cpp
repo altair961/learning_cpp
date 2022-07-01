90850abb2353cf17c1f0a3b9a2728c7904bd004f
altair961 <altair89@list.ru>
2022-07-01 18:59:23 +0300

    Server was enabled to listen

--- chatExampleWithSockets/server.cpp
+++ chatExampleWithSockets/server.cpp
@@ -7,6 +7,9 @@
 #include <netdb.h>
 #include <netinet/in.h>
 
+#define ERROR_S "SERVER ERROR: "
+#define DEFAULT_PORT 1601
+
 int main(int argc, char const* argv[]) {
 	int client;
 	int server;
@@ -15,6 +18,29 @@ int main(int argc, char const* argv[]) {
 
 	client = socket(AF_INET, SOCK_STREAM, 0);
 
-	std::cout << "dsfhbsdjvhb" << std::endl;
+	if(client < 0) {
+		std::cout << ERROR_S << "establishing socket error.\n";
+		exit(0);
+	}
+
+	std::cout << "SERVER: Server socket was successfully created.\n";
+	
+	server_address.sin_port = htons(DEFAULT_PORT);
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htons(INADDR_ANY);
+
+	int ret = bind(client, reinterpret_cast<sockaddr*>(&server_address),
+		sizeof(server_address));
+	
+	if(ret < 0) {
+		std::cout 
+			<< ERROR_S 
+			<< "binding failed.\n";
+		return -1;
+	}
+
+	int size = sizeof(server_address);
+
+	std::cout << "SERVER :" << "Listening clients...\n";
 }
 
