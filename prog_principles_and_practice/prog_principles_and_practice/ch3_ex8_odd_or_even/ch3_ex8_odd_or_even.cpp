37296e41ecd09da5dbffd9412a43ef3f826ef6b9
altair961 <altair89@list.ru>
2022-07-14 08:26:22 +0300

    ch3_ex8_odd_or_even excercise solution was added.

---
+++ prog_principles_and_practice/prog_principles_and_practice/ch3_ex8_odd_or_even/ch3_ex8_odd_or_even.cpp
@@ -0,0 +1,18 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+    cout << "Please enter an int number, separated by enter: ";
+    int number;
+    cin >> number;
+    int remainder;
+    remainder = number % 2;
+
+    if (remainder == 1)
+        cout << "The number: " << number << " is odd.\n";
+
+    if (remainder == 0)
+        cout << "The number: " << number << " is even.\n";
+
+    return 0;
+}
