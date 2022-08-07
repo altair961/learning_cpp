b9f64a936bbdb17354f50980666829e6bc84f103
altair961 <altair89@list.ru>
2022-08-07 16:53:13 +0300

    Google mock was installed.

--- TddCpp/TddCpp/Soundex.Tests/test.cpp
+++ TddCpp/TddCpp/Soundex.Tests/test.cpp
@@ -1,9 +1,18 @@
 #include "pch.h"
+#include <gmock/gmock.h>
 
-class Soundex 
+class Soundex
 {
+public:
+	std::string encode(const std::string& word) const {
+		return "";
+	}
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
 	Soundex soundex;
+
+	auto encoded = soundex.encode("A");
+
+	ASSERT_THAT(encoded, testing::Eq("A"));
 }
