c12cafede8de681ed71c69ca38208dadfe3f7844
altair961 <altair89@list.ru>
2022-08-25 19:25:38 +0300

    ch4_dr6_language_features was implemented. Program reads one input variable and keeps track of the previous value.

---
+++ prog_principles_and_practice/ch4_dr6_language_features/ch4_dr6_language_features.cpp
@@ -0,0 +1,63 @@
+#include "../std_lib_facilities.h"
+
+double GetSmallest(double first, double second)
+{
+	if (first < second)
+		return first;
+
+	return second;
+}
+
+double GetLargest(double first, double second)
+{
+	if (first > second)
+		return first;
+
+	return second;
+}
+
+bool IsDiffSmall(double num1, double num2)
+{
+	double smaller = GetSmallest(num1, num2);
+	double larger = GetLargest(num1, num2);
+	double difference = larger - smaller;
+	double diffThreshold = 1.0 / 100;
+
+	if (difference < diffThreshold)
+		return true;
+
+	return false;
+}
+
+int main()
+{
+	cout << "Please, provide a double number or \"pipe\"-sign \"|\" to terminate input: " << "\n";
+	string input;
+	double prevNum = 0.0;
+	while (cin >> input && input != "|")
+	{
+		double newNum = stod(input);
+
+		cout << "\nnew number: " << newNum << "\n";
+		cout << "previous number: " << prevNum << "\n";
+
+		if (newNum == prevNum)
+			cout << "The numbers are equal\n";
+
+		if (newNum != prevNum)
+		{
+			double smaller = GetSmallest(newNum, prevNum);
+			double larger = GetLargest(newNum, prevNum);
+
+			cout << "The smaller number is: " << smaller << "\n";
+			cout << "The larger number is: " << larger << "\n";
+
+			bool isDiffSmall = IsDiffSmall(newNum, prevNum);
+
+			if (isDiffSmall)
+				cout << "The numbers are almost equal.\n";
+		}
+
+		prevNum = newNum;
+	}
+}
