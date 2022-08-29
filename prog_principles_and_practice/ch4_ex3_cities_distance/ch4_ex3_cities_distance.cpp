1ca02a5dd50162fb5daf420029c558a03ab78652
altair961 <altair89@list.ru>
2022-08-29 08:50:00 +0300

    ch4_ex3_cities_distance was enabled to calculate the smallest, the largest and the mean distance.

--- prog_principles_and_practice/ch4_ex3_cities_distance/ch4_ex3_cities_distance.cpp
+++ prog_principles_and_practice/ch4_ex3_cities_distance/ch4_ex3_cities_distance.cpp
@@ -1,12 +1,54 @@
 #include "../std_lib_facilities.h"
 
+double GetSmallest(vector<double> nums)
+{
+	sort(nums);
+	double smallest = nums.at(0);
+
+	return smallest;
+}
+
+double GetLargest(vector<double> nums) 
+{
+	sort(nums);
+	double largest = nums.at(nums.size() - 1);
+
+	return largest;
+}
+
+double GetMean(double sumOfTerms, double numOfTerms) 
+{
+	double mean = sumOfTerms / numOfTerms;
+
+	return mean;
+}
+
 int main()
 {
 	cout << "Please, provide distances between cities \nalong the given route.\n"
 		<< "Type pipe-sign \"|\" to terminate input when you're done: ";
 
-	for (double distance; cin >> distance;) 
+	vector<double> distances;
+	double totalDistance = 0;
+	double prevDistance = 0;
+	for (double distance = 0; cin >> distance;) 
 	{
 
+		totalDistance += distance;
+		distances.push_back(distance);
+
+		prevDistance = distance;
 	}
+	
+	cout << endl;
+	cout << "The total distance is: " << totalDistance << "\n";
+	
+	double smallestDist = GetSmallest(distances);
+	cout << "The smallest distance between two neighboring cities is: " << smallestDist << "\n";
+
+	double largestDist = GetLargest(distances);
+	cout << "The largest distance between two neighboring cities is: " << largestDist << "\n";
+
+	double meanDistance = GetMean(totalDistance, distances.size());
+	cout << "The mean distance between two neighboring cities is: " << meanDistance << "\n";
 }
