1fab7b991e6eacdbb4b592c6db0ec3a6f3deac1c
altair961 <altair89@list.ru>
2022-10-16 15:28:03 +0300

    Switched project to 32 bit. Created a window.

--- VS Projects/Timber/Timber.cpp
+++ VS Projects/Timber/Timber.cpp
@@ -1,6 +1,14 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
-{
+{ 
+	// Create a video mode object
+	VideoMode vm(800, 600);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	
 	return 0;
 }
