768d83850ac85825b94945c2053ea924f005d24f
altair961 <altair89@list.ru>
2023-01-24 21:59:08 +0300

    Game start/stop was implemented. Timebar was added.

--- VS Projects/Timber/Timber.cpp
+++ VS Projects/Timber/Timber.cpp
@@ -6,6 +6,7 @@ using namespace sf;
 int main()
 { 
 	// Create a video mode object
+	//VideoMode vm(1920, 1080);
 	VideoMode vm(1920, 1080);
 
 	// Create and open a window for the game
@@ -71,6 +72,18 @@ int main()
 	// Variables to control time itself
 	Clock clock;
 
+	// Time bar
+	RectangleShape timeBar;
+	float timeBarStartWidth = 400;
+	float timeBarHeight = 80;
+	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+	timeBar.setFillColor(Color::Red);
+	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 900);
+
+	Time gameTimeTotal;
+	float timeRemaining = 6.0f;
+	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
 	// Track whether the game is running
 	bool paused = true;
 
@@ -110,7 +123,6 @@ int main()
 
 	while (window.isOpen())
 	{
-		score++;
 		/*
 		 ****************************************
 		 Handle the players input
@@ -125,6 +137,10 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Return))
 		{
 			paused = false;
+
+			// Reset the time and the score
+			score = 0;
+			timeRemaining = 5;
 		}
 
 		/*
@@ -136,6 +152,29 @@ int main()
 		{
 			// Measure time
 			Time dt = clock.restart();
+
+			// Subtract from the amount of time remaining
+			timeRemaining -= dt.asSeconds();
+			// size up the time bar
+			timeBar.setSize(Vector2f(timeBarWidthPerSecond *
+				timeRemaining, timeBarHeight));
+
+			if (timeRemaining <= 0.0f)
+			{
+				// Pause the game
+				paused = true;
+
+				// Change the message shown to the player
+				messageText.setString("Out of time!!");
+
+				// Reposition the text based on its new size
+				FloatRect textRect = messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left + 
+					textRect.width /2.0f,
+					textRect.top + 
+					textRect.height / 2.0f);
+				messageText.setPosition(1920 / 2.0f, 1000 / 2.0f);
+			}
 
 			// Setup the bee
 			if (!beeActive)
@@ -289,6 +328,10 @@ int main()
 
 		// Draw the score
 		window.draw(scoreText);
+
+		// Draw the timebar
+		window.draw(timeBar);
+
 		if (paused) 
 		{
 			window.draw(messageText);
