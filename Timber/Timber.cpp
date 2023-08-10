1a2a438af001df6d333a27260b07f3478c48ec5f
altair961 <altair89@list.ru>
2023-08-10 07:56:47 +0300

    A tree sprite was added.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -5,15 +5,35 @@ int main()
 {
     VideoMode vm(1920, 1080);
     RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
-    
+    Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
     while (window.isOpen())
     {
+        // Handle the players input
         if (Keyboard::isKeyPressed(Keyboard::Escape))
         {
             window.close();
         }
+
+        // Update the scene
+
+        // Draw the scene
         window.clear();
-        window.display();
+        window.draw(spriteBackground);
+        window.draw(spriteTree);
+
+        window.display(); // flips from the previously displayed surface to the newly updated(previously hidden) one.
+
     }
     return 0;
 }
