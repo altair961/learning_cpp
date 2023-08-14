fcec7c7fdb371dc834ee1aab69acef1932425781
altair961 <altair89@list.ru>
2023-08-14 08:05:15 +0300

    The bee and the clouds were enabled to move.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -3,67 +3,155 @@ using namespace sf;
 
 int main()
 {
-    VideoMode vm(1920, 1080);
-    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
-    Texture textureBackground;
-    textureBackground.loadFromFile("graphics/background.png");
-    Sprite spriteBackground;
-    spriteBackground.setTexture(textureBackground);
-    spriteBackground.setPosition(0, 0);
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.png");
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
 
-    Texture textureTree;
-    textureTree.loadFromFile("graphics/tree.png");
-    Sprite spriteTree;
-    spriteTree.setTexture(textureTree);
-    spriteTree.setPosition(810, 0);
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
 
-    Texture textureBee;
-    textureBee.loadFromFile("graphics/bee.png");
-    Sprite spriteBee;
-    spriteBee.setTexture(textureBee);
-    spriteBee.setPosition(0, 800);
-    bool beeActive = false;
-    float beeSpeed = 0.0f;
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+	bool beeActive = false;
+	float beeSpeed = 0.0f;
 
-    Texture textureCloud;
-    textureCloud.loadFromFile("graphics/cloud.png");
-    Sprite spriteCloud1;
-    Sprite spriteCloud2;
-    Sprite spriteCloud3;
-    spriteCloud1.setPosition(0, 0);
-    spriteCloud2.setPosition(0, 150);
-    spriteCloud3.setPosition(0, 300);
-    spriteCloud1.setTexture(textureCloud);
-    spriteCloud2.setTexture(textureCloud);
-    spriteCloud3.setTexture(textureCloud);
-    bool cloud1Active = false;
-    bool cloud2Active = false;
-    bool cloud3Active = false;
-    float cloud1Speed = 0.0f;
-    float cloud2Speed = 0.0f;
-    float cloud3Speed = 0.0f;
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 150);
+	spriteCloud3.setPosition(0, 300);
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
 
-    while (window.isOpen())
-    {
-        // Handle the players input
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-        {
-            window.close();
-        }
+	Clock clock;
 
-        // Update the scene
+	while (window.isOpen())
+	{
+		// Handle the players input
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
 
-        // Draw the scene
-        window.clear();
-        window.draw(spriteBackground);
-        window.draw(spriteCloud1);
-        window.draw(spriteCloud2);
-        window.draw(spriteCloud3);
-        window.draw(spriteTree);
-        window.draw(spriteBee);
+		// Update the scene
+		Time deltaTime = clock.restart();
 
-        window.display(); // flips from the previously displayed surface to the newly updated(previously hidden) one.
+		if (!beeActive)
+		{
+			srand((int)time(0));
+			beeSpeed = (rand() % 200) + 200;
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+			beeActive = true;
+		}
+		else
+		{
+			spriteBee.setPosition(
+				spriteBee.getPosition().x - (beeSpeed * deltaTime.asSeconds()),
+				spriteBee.getPosition().y);
 
-    }
-    return 0;
+			if (spriteBee.getPosition().x < -100)
+			{
+				beeActive = false;
+			}
+		}
+
+		if (!cloud1Active)
+		{
+			srand((int)time(0) * 10);
+			cloud1Speed = (rand() % 200);
+			srand((int)time(0) * 10);
+			float height = (rand() % 150);
+			spriteCloud1.setPosition(-200, height);
+			cloud1Active = true;
+		}
+		else
+		{
+			spriteCloud1.setPosition(
+				spriteCloud1.getPosition().x + (cloud1Speed * deltaTime.asSeconds()),
+				spriteCloud1.getPosition().y);
+
+			if (spriteCloud1.getPosition().x > 1920)
+			{
+				cloud1Active = false;
+			}
+		}
+
+		if (!cloud2Active)
+		{
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(-200, height);
+			cloud2Active = true;
+		}
+		else
+		{
+			spriteCloud2.setPosition(
+				spriteCloud2.getPosition().x + (cloud2Speed * deltaTime.asSeconds()),
+				spriteCloud2.getPosition().y);
+
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				cloud2Active = false;
+			}
+		}
+
+		if (!cloud3Active)
+		{
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(-200, height);
+			cloud3Active = true;
+		}
+		else
+		{
+			spriteCloud3.setPosition(
+				spriteCloud3.getPosition().x +
+				(cloud3Speed * deltaTime.asSeconds()),
+				spriteCloud3.getPosition().y);
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				cloud3Active = false;
+			}
+		}
+
+		// Draw the scene
+		window.clear();
+		window.draw(spriteBackground);
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+		window.draw(spriteTree);
+		window.draw(spriteBee);
+
+		window.display(); // flips from the previously displayed surface to the newly updated(previously hidden) one.
+
+	}
+	return 0;
 }
