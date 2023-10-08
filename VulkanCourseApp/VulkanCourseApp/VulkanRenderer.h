bfc9d9e703bf7b36f94316cc15e234944a86d635
altair961 <altair89@list.ru>
2023-10-08 18:53:43 +0300

    Basic VulkanRenderer.h and VulkanRenderer.cpp were added to the project.

--- VulkanCourseApp/VulkanCourseApp/VulkanRenderer.h
+++ VulkanCourseApp/VulkanCourseApp/VulkanRenderer.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+
+class VulkanRenderer 
+{
+public:
+	VulkanRenderer();
+	~VulkanRenderer();
+};
