d48053f95efcdaf1ecfff7665fc839b0c69d5e57
altair961 <altair89@list.ru>
2023-10-13 17:25:25 +0300

    Empty createGraphicsPipeline was added

--- VulkanCourseApp/VulkanCourseApp/main.cpp
+++ VulkanCourseApp/VulkanCourseApp/main.cpp
@@ -346,6 +346,9 @@ private:
 		}
 	}
 
+	void createGraphicsPipeline() {
+
+	}
 	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
 		for (const auto& availableFormat : availableFormats) {
 			if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
