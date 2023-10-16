7bebe9d44fec92482ddb1406d61c1a616cd0733f
altair961 <altair89@list.ru>
2023-10-16 21:06:44 +0300

    Vulkan app skeleton was defined

--- VulkanCourseApp/VulkanCourseApp/main.cpp
+++ VulkanCourseApp/VulkanCourseApp/main.cpp
@@ -1,5 +1,44 @@
+#include <vulkan/vulkan.h>
+
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+private:
+    void initVulkan()
+    {
+        std::cout << "at initVulkan()" << std::endl;
+    }
+    void mainLoop() 
+    {
+        std::cout << "at mainLoop()" << std::endl;
+    }
+    void cleanup()
+    {
+        std::cout << "at cleanup()" << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello world";
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
+
