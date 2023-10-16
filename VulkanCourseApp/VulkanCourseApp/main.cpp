644f2a5357fee5b0fe247e9e34c4447d262822fb
altair961 <altair89@list.ru>
2023-10-16 21:55:28 +0300

    Vulkan instance creation was implemented and invoked

--- VulkanCourseApp/VulkanCourseApp/main.cpp
+++ VulkanCourseApp/VulkanCourseApp/main.cpp
@@ -20,6 +20,7 @@ public:
     }
 private:
     GLFWwindow* window;
+    VkInstance instance;
     void initWindow() 
     {
         std::cout << "at initWindow()" << std::endl;
@@ -34,6 +35,36 @@ private:
     void initVulkan()
     {
         std::cout << "at initVulkan()" << std::endl;
+
+        createInstance();
+    }
+    void createInstance() 
+    {
+        std::cout << "at createInstance()" << std::endl;
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0); 
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0); 
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) 
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
     void mainLoop() 
     {
