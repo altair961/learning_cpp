6af168f5d1f8dac777d354e76d8d608897283f6e
altair961 <altair89@list.ru>
2023-10-16 21:27:35 +0300

    GLFW window creation and destroying were implemented

--- VulkanCourseApp/VulkanCourseApp/main.cpp
+++ VulkanCourseApp/VulkanCourseApp/main.cpp
@@ -1,19 +1,36 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication
 {
 public:
     void run()
-    {
+    {   
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 private:
+    GLFWwindow* window;
+    void initWindow() 
+    {
+        std::cout << "at initWindow()" << std::endl;
+
+        glfwInit();
+        
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
     void initVulkan()
     {
         std::cout << "at initVulkan()" << std::endl;
@@ -21,10 +38,18 @@ private:
     void mainLoop() 
     {
         std::cout << "at mainLoop()" << std::endl;
+
+        while (!glfwWindowShouldClose(window)) 
+        {
+            glfwPollEvents();
+        }
     }
     void cleanup()
     {
         std::cout << "at cleanup()" << std::endl;
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 };
 
