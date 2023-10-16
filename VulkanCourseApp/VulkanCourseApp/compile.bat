bf3ae7ccffa848bbb8f053ad930f04c06d0472e7
altair961 <altair89@list.ru>
2023-10-16 21:01:21 +0300

    Shaders were reset

--- VulkanCourseApp/VulkanCourseApp/compile.bat
+++ VulkanCourseApp/VulkanCourseApp/compile.bat
@@ -1,3 +1,3 @@
-"D:\libs\VulkanSDK\1.3.261.1\Bin\glslc.exe" shader.vert -o vert.spv
-"D:\libs\VulkanSDK\1.3.261.1\Bin\glslc.exe" shader.frag -o frag.spv
+"D:\libs\VulkanSDK\1.3.261.1\Bin\glslc.exe" shader.vert -o .\shaders\vert.spv
+"D:\libs\VulkanSDK\1.3.261.1\Bin\glslc.exe" shader.frag -o .\shaders\frag.spv
 pause
