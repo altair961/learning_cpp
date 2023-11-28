e6802a11a15d1a8c6f1c1b3fa842afc4dccb14d1
altair961 <altair89@list.ru>
2023-11-28 07:33:37 +0300

    SierpinskiTriangle vulkan project was created.

---
+++ VulkanCourseApp/SierpinskiTriangle/compileShaders.bat
@@ -0,0 +1,3 @@
+"D:\libs\VulkanSDK\1.3.268.0\Bin\glslc.exe" shader.vert -o .\shaders\vert.spv
+"D:\libs\VulkanSDK\1.3.268.0\Bin\glslc.exe" shader.frag -o .\shaders\frag.spv
+pause
---
+++ VulkanCourseApp/SierpinskiTriangle/first_app.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "lve_window.h"
+#include "lve_pipeline.h"
+#include "lve_device.h"
+#include "lve_swap_chain.h"
+#include "lve_model.h"
+
+// std
+#include <memory>
+#include <vector>
+
+namespace lve {
+	class FirstApp {
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
+
+		FirstApp();
+		~FirstApp();
+
+		FirstApp(const FirstApp&) = delete;
+		FirstApp& operator=(const FirstApp&) = delete;
+
+
+		void run();
+
+	private:
+		void loadModels();
+		void createPipelineLayout();
+		void createPipeline();
+		void createCommandBuffers();
+		void drawFrame();
+
+		LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
+		LveDevice lveDevice{ lveWindow };
+		LveSwapChain lveSwapChain{ lveDevice, lveWindow.getExtent() };
+		std::unique_ptr<LvePipeline> lvePipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
+		std::unique_ptr<LveModel> lveModel;
+	};
+}
