8a35dbdb622246d671cd1ae552c1191e0227fc48
altair961 <altair89@list.ru>
2023-12-25 07:19:49 +0300

    Exercise1.1 project was created.

---
+++ Creating_3D-Games/Chapter1/Exercise1.1/Game.cpp
@@ -0,0 +1,280 @@
+#include "Game.h"
+
+void Game::RunLoop()
+{
+	while (mIsRunning)
+	{
+		// Process Inputs
+		ProcessInput();
+		// Update Game World
+		UpdateGame();
+		// Generate Outputs
+		GenerateOutput();
+	}
+}
+Game::Game() {
+	mWindow = nullptr;
+	mIsRunning = true;
+	mBallPos = { 1024 / 2, 768 / 2 };
+	mPaddlePos = { static_cast<float>(thickness) / 2 + thickness, 768 / 2 };
+	mTicksCount = 0;
+	mPaddleDir = 0;
+}
+
+void Game::ProcessInput() 
+{
+	SDL_Event event;
+	// While there are still events in the queue
+	while (SDL_PollEvent(&event)) 
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			mIsRunning = false;
+			break;
+		}
+	}
+
+	// Get the state of keyboard
+	const Uint8* state = SDL_GetKeyboardState(NULL);
+	// if escape is pressed, also end loop
+	if (state[SDL_SCANCODE_ESCAPE])
+	{
+		mIsRunning = false;
+	}
+
+	mPaddleDir = 0;
+	if (state[SDL_SCANCODE_W]) 
+	{
+		mPaddleDir -= 1;
+	}
+	if (state[SDL_SCANCODE_S])
+	{
+		mPaddleDir += 1;
+	}
+}
+void Game::UpdateGame() {
+	// Wait until 16ms has elapsed since last frame
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16))
+		;
+
+	// Delta time is the difference in ticks from last frame
+	// Converted to seconds
+	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f; // deltTime is in seconds
+	
+	// Update tick counts (for next frame)
+	mTicksCount = SDL_GetTicks();
+	
+
+	// Clamp maximum delta time value
+	if (deltaTime > 0.05f) 
+	{
+		deltaTime = 0.5f;
+	}
+
+	if (mPaddleDir != 0)
+	{
+		mPaddlePos.y += mPaddleDir * 300.0f * deltaTime;
+		// make sure paddle doesn't go off screen
+		if (mPaddlePos.y < (paddleH / 2.0f + thickness))
+			mPaddlePos.y = paddleH / 2.0f + thickness;
+		else if (mPaddlePos.y > (768.0f - paddleH / 2.0f - thickness))
+		{
+			mPaddlePos.y = (768.0f - paddleH / 2.0f - thickness);
+		}
+	}
+
+	mBallPos.x += mBallVel.x * deltaTime;
+	mBallPos.y += mBallVel.y * deltaTime;
+
+	// Did the ball collide with the top wall?
+	if (mBallPos.y <= (thickness + thickness/2)&& mBallVel.y < 0.0f)
+	{
+		mBallVel.y *= -1;
+	}
+
+	// Did the ball collide with the bottom wall?
+	if (mBallPos.y >= 768 - thickness - (thickness /2) && mBallVel.y > 0.0f)
+	{
+		mBallVel.y *= -1;
+	}
+
+	// Did the ball collide with the right wall?
+	if (mBallPos.x >= (1024 - thickness - (thickness / 2)) && mBallVel.x > 0.0f)
+	{
+		mBallVel.x *= -1;
+	}
+
+	// Did we intersect with the paddle?
+	if (
+		mBallPos.y > (mPaddlePos.y - (paddleH / 2))
+		&& mBallPos.y < (mPaddlePos.y + (paddleH / 2))
+		&&
+		// Ball is at the correct x-position
+		mBallPos.x <= (mPaddlePos.x + thickness) && mBallPos.x >= mPaddlePos.x &&
+		// the ball is moving to the left
+		mBallVel.x < 0.0f)
+	{
+		mBallVel.x *= -1;
+	}
+
+	if (BallComesFromTop() &&
+		BallIsHigherThanPaddle() &&
+		BallBottomHitPaddleTopAlready() &&
+		BallIsAlignedWithPaddleXAxis())
+	{
+		mBallVel.y *= -1;
+	}
+
+	if (!BallComesFromTop() &&
+		!BallIsHigherThanPaddle() &&
+		BallTopHitPaddleBottomAlready() &&
+		BallIsAlignedWithPaddleXAxis())
+	{
+		mBallVel.y *= -1;
+	}
+
+	if (BallMovedOffScreen()) 
+	{
+		GameOver();
+	}
+}
+
+bool Game::BallMovedOffScreen()
+{
+	auto result = mBallPos.x <= 0;
+	return result;
+}
+
+void Game::GameOver()
+{
+	mIsRunning = false;
+}
+
+bool Game::BallIsAlignedWithPaddleXAxis() 
+{
+	auto ballLeftSideX = mBallPos.x - thickness / 2;
+	auto ballRightSideX = mBallPos.x + thickness / 2;
+	auto paddleRightSideX = mPaddlePos.x + thickness / 2;
+	auto paddleLeftSideX = mPaddlePos.x - thickness / 2;
+
+	if (ballLeftSideX > paddleRightSideX)
+		return false;
+
+	if (ballRightSideX < ballLeftSideX)
+		return false;
+
+	if (ballLeftSideX <= paddleRightSideX && 
+		ballLeftSideX >= paddleLeftSideX)
+		return true;
+
+	if (ballRightSideX <= paddleRightSideX &&
+		ballRightSideX >= paddleLeftSideX)
+		return true;
+
+	return false;
+}
+
+bool Game::BallTopHitPaddleBottomAlready() 
+{
+	auto ballTopSideY = mBallPos.y - thickness / 2;
+	auto paddleBottomSideY = mPaddlePos.y + paddleH / 2;
+	auto result = ballTopSideY < paddleBottomSideY;
+
+	return result;
+}
+
+bool Game::BallBottomHitPaddleTopAlready()
+{
+	auto ballBottomSideY = mBallPos.y + thickness / 2;
+	auto paddleTopSideY = mPaddlePos.y - paddleH / 2;
+	auto result = ballBottomSideY >= paddleTopSideY;
+
+	return result;
+}
+
+bool Game::BallIsHigherThanPaddle() 
+{
+	auto result = mBallPos.y < mPaddlePos.y;
+	
+	return result;
+}
+
+bool Game::BallComesFromTop() 
+{
+	auto result = mBallVel.y > 0.0f;
+
+	return result;
+}
+
+void Game::GenerateOutput() {
+	SDL_SetRenderDrawColor(mRenderer, 57, 83, 164, 255);
+	SDL_RenderClear(mRenderer);
+
+	SDL_Rect topWall{ 0, 0, 1024, thickness };
+	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(mRenderer, &topWall);
+
+	SDL_Rect bottomWall{ 0, 768 - thickness, 1024, thickness };
+	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(mRenderer, &bottomWall);
+
+	SDL_Rect rightWall{ 1024 - thickness, 0, thickness, 768 };
+	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(mRenderer, &rightWall);
+
+	SDL_Rect ball{
+		static_cast<int>(mBallPos.x - thickness / 2),
+		static_cast<int>(mBallPos.y - thickness / 2),
+		thickness,
+		thickness
+	};
+	SDL_RenderFillRect(mRenderer, &ball);
+
+	SDL_Rect paddle{
+		static_cast<int>(mPaddlePos.x - thickness / 2),
+		mPaddlePos.y - paddleH / 2,
+		thickness,
+		paddleH
+	};
+	SDL_RenderFillRect(mRenderer, &paddle);
+
+	SDL_RenderPresent(mRenderer);
+}
+bool Game::Initialize() {
+	int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+	if (sdlResult != 0) 
+	{
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+	mWindow = SDL_CreateWindow(
+		"Game Programming in C++ (Chapter 1)", // Window title
+		100,	// Top left x-coordinate of window
+		100,	// Top left y-coordinate of window
+		1024,	// Width of window
+		768,	// Height of window
+		0		// Flags (0 for no flags set)
+	);
+	if (!mWindow) 
+	{
+		SDL_Log("Failed to create window: %s", SDL_GetError());
+		return false;
+	}
+	mRenderer = SDL_CreateRenderer(
+		mWindow, // Window to create renderer for
+		-1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+	);
+	
+	if (!mRenderer)
+	{
+		SDL_Log("Failed to create renderer: %s", SDL_GetError());
+		return false;
+	}
+}
+void Game::Shutdown() {
+	SDL_DestroyRenderer(mRenderer);
+	SDL_DestroyWindow(mWindow);
+	SDL_Quit();
+}
---
+++ Creating_3D-Games/Chapter1/Exercise1.1/Game.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <SDL.h>
+
+class Game {
+public:
+	Game();
+	bool Initialize();
+	void RunLoop();
+	void Shutdown();
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+	bool BallComesFromTop();
+	bool BallBottomHitPaddleTopAlready();
+	bool BallTopHitPaddleBottomAlready();
+	bool BallIsHigherThanPaddle();
+	bool BallIsAlignedWithPaddleXAxis();
+	bool BallMovedOffScreen();
+	void GameOver();
+
+	SDL_Window* mWindow;
+	bool mIsRunning;
+	SDL_Renderer* mRenderer;
+	const int thickness = 15;
+	struct Vector2 
+	{
+		float x;
+		float y;
+	};
+	Vector2 mPaddlePos
+	{
+	};
+	Vector2 mBallPos
+	{
+	};
+	Uint32 mTicksCount;
+	int mPaddleDir;
+	const int paddleH = thickness * 6.5;
+	Vector2 mBallVel{ 200, 100};
+};
