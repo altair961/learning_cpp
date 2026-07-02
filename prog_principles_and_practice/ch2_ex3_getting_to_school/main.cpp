47a80a06960c0a0be4b3ed79698a0c04b429aa60
altair961 <altair89@list.ru>
2026-07-03 00:50:14 +0300

    Implement ch2_ex3_getting_to_school

---
+++ prog_principles_and_practice/ch2_ex3_getting_to_school/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "exit through the door" << endl;
+    cout << "use lift to reach first floor" << endl;
+    cout << "go striaght along the road" << endl;
+    cout << "turn left" << endl;
+    cout << "three steps forward" << endl;
+    cout << "open the school door" << endl;
+
+    return 0;
+}
