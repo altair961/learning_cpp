19ad852f3171de6d2e402a552c6c794cc590b3ff
altair961 <altair89@list.ru>
2026-07-07 23:11:27 +0300

    Improve ch3_tt4_for

--- prog_principles_and_practice/ch3_tt4_for/main.cpp
+++ prog_principles_and_practice/ch3_tt4_for/main.cpp
@@ -5,5 +5,11 @@ int main()
     for(int i = 97; i <= 122; ++i)
         cout << char(i) << '\t' << i << '\n';
 
+    for(int i = 65; i <= 90; ++i)
+        cout << char(i) << '\t' << i << '\n';
+
+    for(int i = 48; i <= 57; ++i)
+        cout << char(i) << '\t' << i << '\n';
+
     return 0;
 }
