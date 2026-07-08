2f6b9d878ca87362e86e78192c63fe6eafdd4fb0
altair961 <altair89@list.ru>
2026-07-08 19:00:50 +0300

    Fix ch3_dr1_language_features

--- prog_principles_and_practice/ch3_dr1_language_features/main.cpp
+++ prog_principles_and_practice/ch3_dr1_language_features/main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    string value1, value2;
-    while(cin >> value1 && value1 != "|"
-          && cin >> value2 && value2 != "|") {
+    int value1, value2;
+    while(cin >> value1 && value1 != '|'
+          && cin >> value2 && value2 != '|') {
         cout << value1 << " " << value2 << '\n';
     }
 
