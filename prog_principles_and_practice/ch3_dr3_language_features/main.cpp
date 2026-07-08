bc76c3b101681e86943050032cce13840d08aaae
altair961 <altair89@list.ru>
2026-07-08 19:22:19 +0300

    Implement ch3_dr3_language_features/

---
+++ prog_principles_and_practice/ch3_dr3_language_features/main.cpp
@@ -0,0 +1,22 @@
+#include "../PPPheaders.h"
+
+int main()
+{
+    int value1, value2;
+    while(cin >> value1 && value1 != '|'
+          && cin >> value2 && value2 != '|') {
+
+        if(value1 > value2) {
+            cout << "The smaller number is: " << value2 << '\n'
+            << "and the larger number is: " << value1;
+        } else if(value1 < value2) {
+            cout << "The smaller number is: " << value1 << '\n'
+            << "and the larger number is: " << value2;
+        } else if (value1 == value2){
+            cout << "the numbers are equal\n";
+        }
+    }
+
+
+    return 0;
+}
